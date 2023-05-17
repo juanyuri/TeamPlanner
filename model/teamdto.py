8c6256ef19824230675798c680b4eee98dc396d6
Yuri <jdsanchez19@esei.uvigo.es>
2023-05-17 20:15:31 +0200

    Improved Teams DTO

--- model/teamdto.py
+++ model/teamdto.py
@@ -1,12 +1,12 @@
-class Team:
+class TeamDTO:
     def __init__(self, nombre, descripcion, codigo, fecha, autor):
         self.__nombre = nombre
         self.__codigo = codigo
         self.__descripcion = descripcion
         self.__fecha = fecha
         self.__autor = autor
-        self.__rating = 0
-        
+        self.__rating = 0.0
+    
     @property
     def nombre(self):
         return self.__nombre
@@ -30,3 +30,7 @@ class Team:
     def __str__(self):
         return "Equipo [id=" + self.__codigo + ", descripcion=" + self.__descripcion + ", fechaCreacion=" + self.__fecha \
                 + ", creador=" + self.__autor + ", rating=" + self.__rating + "]"
+    
+    @staticmethod
+    def find(s: sirope.Sirope, codigo: str) -> "TeamDTO":
+        return s.find_first(TeamDTO, lambda t: t.codigo == codigo)
